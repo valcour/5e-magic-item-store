47eb0909f417b8005c9af436f08ff13032c87a25
Val <val@Nonesuch.local>
2021-07-16 15:22:49 -0400

    utils

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
-// yea!! code!!
+/*
+* get ist of magic items from api
+*/ 
+function get_item_list() {
+// !!!
+}
+
+/* 
+* role a die
+*  imput:number of sids on the die
+* output: number that was roled
+*/
+function role_die(sides) {
+    // !!!
+    // return a randome number between 1 and number of sides
+}
+
+
+
+
+/* 
+* role a dice
+*  imput:number of sids on the dice
+* number of dice to role
+* output: number that was roled
+*/
+function role_dice(dice_count, sides) {
+    // !!!
+    // repeat roll_die(sides) dice_count number of times
+    // return the total of the result
+}
+
+
+
+
+
+
+
