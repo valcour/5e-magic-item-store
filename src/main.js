53ffcac6a595b5bbfce0601baec3d57da0e76eac
valcour <valliantkeepin@gmail.com>
2021-07-21 12:13:53 -0400

    outline of most of the things

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
+// a list of 5e magic item rarities
+const rarity = {
+    "COMMON"   :  "common",  
+    "UNCOMMON" :  "uncommon",
+    "RARE"     :  "rare",
+    "VERY_RARE":  "vere_rare",
+    "LEGENDARY":  "legendary"
+};
 
+// for each rarity, roll dice for that fore the number of items in that rareity
+function get_rarity_counts (rarity_dice) {
+// !!!
+};
 
+// fore each item role dice for price based on rarity
+function get_item_prices(rarity_counts, price_dice) {
+// !!!
+};
 
+// for each item assign randome name from list, matching rarity
+function get_item_names(item_list, item_prices) {
+// name dupes are ok
+// !!!
+};
 
-
-
-
-
+// display items + details
+function diplay_all(inventory){
+// !!!
+};
